c7335f603dc5333a70ebe2ba8f8817b6d96e3f54
raidiizx <Auth0.zzz@gmail.com>
2024-03-16 23:37:27 +0700

    fixed some bug

--- js/index.js
+++ js/index.js
@@ -3,14 +3,25 @@ function calC() {
     const thisYear = new Date();
     console.log(thisYear.getFullYear());
     // get user birth year
-    const value2 = document.getElementById("user-birth-year").value;
+    const x = document.getElementById("user-birth-year").value;
     // (this year)-(user birth year)
-    const result = thisYear.getFullYear() - value2
+    const result = thisYear.getFullYear() - x
     // print value to paragraph
     //create paragraph with id
     //get element 
     const paragraphElement = document.getElementById('age');
     //put result in innerhtml property
-    paragraphElement.innerHTML = `${result} years olds`;
     // if value over thisyear alert user do not put over thisyear
-}
+    if (result < 0) {
+        alert(`Do not enter over ${thisYear.getFullYear()}`);
+    }
+    if (x <= 0) {
+        alert(`Please fill the empty form`);
+    }
+    if (x > thisYear.getFullYear()) {
+        paragraphElement.innerHTML = ""
+    }
+    else {
+        paragraphElement.innerHTML = `${result} years olds`;
+    }
+}   
